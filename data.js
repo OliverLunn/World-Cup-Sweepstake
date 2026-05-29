e49aaa1c0330e56da5c0ee4fb193eb362e439318
OliverLunn <136323103+OliverLunn@users.noreply.github.com>
2026-05-29 10:15:54 +0100

    Update sweepstakeData with additional players

--- data.js
+++ data.js
@@ -164,22 +164,24 @@ const teamCodes = {
 // ==========================================
 // Each player has an array of 3 teams they picked in the sweepstake. We will update these with actual team names once we fetch the data from the API
 const sweepstakeData = [
-    { player: "Tom Antolin", teams: ["TBD","TBD", "TBD"], status: "In Tournament" },
-    { player: "Anna Dalmasso", teams: ["TBD","TBD", "TBD"], status: "In Tournament" },
-    { player: "Jake Dorman", teams: ["TBD","TBD", "TBD"], status: "In Tournament" },
-    { player: "Jacob Campbell", teams: ["TBD","TBD", "TBD"], status: "In Tournament" },
-    { player: "Breagh Macpherson", teams: ["TBD","TBD", "TBD"], status: "In Tournament" },
-    { player: "Dan Ford", teams: ["TBD","TBD", "TBD"], status: "In Tournament" },
-    { player: "Dan Ferring", teams: ["TBD","TBD", "TBD"], status: "In Tournament" },
-    { player: "Rayff De Souza", teams: ["TBD","TBD", "TBD"], status: "In Tournament" },
-    { player: "Greg Humphrey-Evans", teams: ["TBD","TBD", "TBD"], status: "In Tournament" },
-    { player: "Oliver Lunn", teams: ["TBD","TBD", "TBD"], status: "In Tournament" },
-    { player: "Tyler Rolfe", teams: ["TBD","TBD", "TBD"], status: "In Tournament" },
-    { player: "Adela Fernandez", teams: ["TBD","TBD", "TBD"], status: "In Tournament" },
-    { player: "Guillaume Hewitt", teams: ["TBD","TBD", "TBD"], status: "In Tournament" },
-    { player: "Martin Valincius", teams: ["TBD","TBD", "TBD"], status: "In Tournament" },
-    { player: "Sameer Jin", teams: ["TBD","TBD", "TBD"], status: "In Tournament" },
-    { player: "Hattie O'Brien", teams: ["TBD","TBD", "TBD"], status: "In Tournament" }
+    { player: "Tom Antolin", teams: ["TBD","TBD"], status: "In Tournament" },
+    { player: "Anna Dalmasso", teams: ["TBD","TBD"], status: "In Tournament" },
+    { player: "Jake Dorman", teams: ["TBD","TBD"], status: "In Tournament" },
+    { player: "Jacob Campbell", teams: ["TBD","TBD"], status: "In Tournament" },
+    { player: "Breagh Macpherson", teams: ["TBD","TBD"], status: "In Tournament" },
+    { player: "Dan Ford", teams: ["TBD","TBD"], status: "In Tournament" },
+    { player: "Dan Ferring", teams: ["TBD","TBD"], status: "In Tournament" },
+    { player: "Rayff De Souza", teams: ["TBD","TBD"], status: "In Tournament" },
+    { player: "Greg Humphrey-Evans", teams: ["TBD","TBD"], status: "In Tournament" },
+    { player: "Oliver Lunn", teams: ["TBD","TBD"], status: "In Tournament" },
+    { player: "Tyler Rolfe", teams: ["TBD","TBD"], status: "In Tournament" },
+    { player: "Adela Fernandez", teams: ["TBD","TBD"], status: "In Tournament" },
+    { player: "Guillaume Hewitt", teams: ["TBD","TBD"], status: "In Tournament" },
+    { player: "Martin Valincius", teams: ["TBD","TBD"], status: "In Tournament" },
+    { player: "Sameer Jin", teams: ["TBD","TBD"], status: "In Tournament" },
+    { player: "Hattie O'Brien", teams: ["TBD","TBD"], status: "In Tournament" },
+    { player: "Anna Beer", teams: ["TBD","TBD"], status: "In Tournament" }.
+    { player: "Ewan Kennett", teams: ["TBD","TBD"], status: "In Tournament" }
 ];
 
 // ==========================================
